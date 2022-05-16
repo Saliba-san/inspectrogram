834569b538efc996d8c775bdf5c66e96130eecd8
Tulio Dias <tuliodiasa@hotmail.com>
2022-05-15 21:14:04 -0300

    Implementando model para a tabela de Imagens

--- back/src/data/database.sql
+++ back/src/data/database.sql
@@ -1,8 +1,17 @@
-CREATE TABLE IF NOT EXISTS musics (
+CREATE TABLE IF NOT EXISTS music (
     id INTEGER NOT NULL
     music TEXT NOT NULL
-    artist TEXT
     path TEXT NOT NULL
     PRIMARY KEY(id)
 )
 
+CREATE TABLE IF NOT EXISTS images (
+    id INTEGER NOT NULL
+    audio_id INTEGER NOT NULL
+    window_size INTEGER
+    window_type TEXT
+    hop_size INTEGER
+    color TEXT
+    path TEXT NOT NULL
+
+)
--- back/src/models.py
+++ back/src/models.py
@@ -4,11 +4,11 @@ class Musics(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     music = db.Column(db.Text, nullable=False)
     artist = db.Column(db.Text, nullable=True)
-    path = db.Column(db.Text, nullable=False)
+    path = db.Column(db.Text, nullable=True)
 
 
     def __repr__(self):
-        return "<Musics %r>" % self.music
+        return "<Music %r, Name %r>" % self.id, self.music
 
 # Generate marshmallow Schemas from your models
 class MusicsSchema(ma.Schema):
@@ -17,5 +17,26 @@ class MusicsSchema(ma.Schema):
         fields = ("id","music", "artist", "path")
 
 
+class Images(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    music_id = db.Column(db.Text, db.ForeignKey('musics.id'))
+    window_size = db.Column(db.Integer, nullable=False)
+    hop_size  = db.Column(db.Integer, nullable=False)
+    window_type = db.Column(db.Text, nullable=True)
+    color = db.Column(db.Text, nullable=False)
+    path = db.Column(db.Text, nullable=True)
+
+    def __repr__(self):
+        return "<Image %r, From Music %r>" % self.id, self.music_id
+
+# Generate marshmallow Schemas from your models
+class ImagesSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ("id","music_id", "window_size", "hop_size", "window_type", "color", "path")
+
 musics_schema = MusicsSchema()
 musics_schema = MusicsSchema(many=True)
+
+images_schema = ImagesSchema()
+images_schema = ImagesSchema(many=True)
