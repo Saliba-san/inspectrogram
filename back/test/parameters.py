2c568aaac06cc30776630c9c3c3f356c268a8f0e
Lara <gtlchaves@gmail.com>
2022-05-21 13:34:14 -0300

    Atualiza parametros

--- back/test/parameters.py
+++ back/test/parameters.py
@@ -1,5 +1,13 @@
 class Stft:
-    def __init__(self, frame_size=1024, hop_length=512, window="hann"):
+
+    def __init__(self, frame_size=1024, hop_length=512, window="hann",
+                 mode="power", cmap="prism", sr=None, db=True, freq_slice=None):
+
+        self.mode = mode
+        self.window = window
         self.frame_size = frame_size
         self.hop_length = hop_length
-        self.window = window
+        self.cmap = cmap
+        self.sr = sr
+        self.db = db
+        self.freq_slice = freq_slice
